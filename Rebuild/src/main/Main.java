ac7885ce7dc5337815b4aa6731a807d198d004dd
JaGUaRsPork <andresherrerof@gmail.com>
2018-04-17 17:48:53 +0200

    testing
    testing jenkins

--- Rebuild/src/main/Main.java
+++ Rebuild/src/main/Main.java
@@ -6,7 +6,7 @@ import view.MainFrame;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		System.out.println("TESTING JENKINS");
 		MainFrame.getInstance().setVisible(true);;
 		
 		//TESTING GITHUB COMMITS
