eaae6c796925e3550a25077ea029da8d259bdb34
JaGUaRsPork <andresherrerof@gmail.com>
2018-04-18 13:25:58 +0200

    SYNC TEST

---
+++ Rebuild/src/components/Task.java
@@ -0,0 +1,19 @@
+package components;
+
+import java.util.Calendar;
+
+public class Task {
+	
+	private Calendar start;
+	private Calendar end;
+	private Calendar creation;
+	private String title;
+	private String description;
+	
+	public Task(Calendar start, Calendar end, Calendar creation, String title, String description){
+		this.start = start;
+		
+		
+	}
+
+}
