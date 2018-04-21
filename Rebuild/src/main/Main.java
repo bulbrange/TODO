befd130a3245c9a11da2482c0e369da12387f11e
JaGUaRsPork <andresherrerof@gmail.com>
2018-04-21 14:41:07 +0200

    LAST PUSH ANTES DE COMER

--- Rebuild/src/main/Main.java
+++ Rebuild/src/main/Main.java
@@ -6,7 +6,7 @@ import view.MainFrame;
 public class Main {
 	// NUEVO COMENTARIO
 
-	
+	//ESTO ES OTRO COMENTARIO NUEVO
 	public static void main(String[] args) {
 		System.out.println("TESTING JENKINS");
 		MainFrame.getInstance().setVisible(true);;
