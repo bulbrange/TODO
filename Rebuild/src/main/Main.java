787b45bcc3a965cfde12462e221125777442418b
Fallow&Peppetter <jaimemg@outlook.com>
2018-04-21 12:31:53 +0200

    Comentrario TEST

--- Rebuild/src/main/Main.java
+++ Rebuild/src/main/Main.java
@@ -5,6 +5,8 @@ import view.MainFrame;
 
 public class Main {
 
+	//COMERME LOS HUEVOS
+	
 	public static void main(String[] args) {
 		System.out.println("TESTING JENKINS");
 		MainFrame.getInstance().setVisible(true);;
