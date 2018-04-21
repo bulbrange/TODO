6185cd5d528e51f37a0b871eb0f1ea9e5fcf202b
JaGUaRsPork <andresherrerof@gmail.com>
2018-04-21 14:31:08 +0200

    final test

--- Rebuild/src/main/Main.java
+++ Rebuild/src/main/Main.java
@@ -4,12 +4,7 @@ package main;
 import view.MainFrame;
 
 public class Main {
-	//JANDE MORE NAWERRRRR
-	//COMERME LOS HUEVOS
-
-	//TOKOTO
-	//PUSHITIPUSH AL FINAL TODO FUNCIONA
-	//mi arrays  fastfooteable
+	// NUEVO COMENTARIO
 
 	
 	public static void main(String[] args) {
