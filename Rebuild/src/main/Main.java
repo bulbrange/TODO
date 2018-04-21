38fd7a22ed140277c6bdabd2b65ceb4420e8303f
Esberfes <casas222@hotmail.com>
2018-04-21 14:12:08 +0200

    Update .gitignore

--- Rebuild/src/main/Main.java
+++ Rebuild/src/main/Main.java
@@ -6,6 +6,7 @@ import view.MainFrame;
 public class Main {
 	//JANDE MORE NAWERRRRR
 	//COMERME LOS HUEVOS
+	//mi arrays  fastfooteable
 	
 	public static void main(String[] args) {
 		System.out.println("TESTING JENKINS");
